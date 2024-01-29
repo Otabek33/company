70a774a54362314d7085ba6c0dda240681ca7367
Otabek33 <kebatotabek33@gmail.com>
2024-01-29 12:14:35 +0500

    translation added

--- sftsgroup/urls.py
+++ sftsgroup/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
-                  path("admin/", admin.site.urls),
-                  path("", include("base.urls", namespace="main")),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = i18n_patterns(
+    path("admin/", admin.site.urls),
+    path("", include("base.urls", namespace="main")),
+)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [
     path("i18n/", include("django.conf.urls.i18n")),
