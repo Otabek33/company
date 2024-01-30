eb656e16378df9abf3bfc68213631d2bd2733632
Otabek33 <kebatotabek33@gmail.com>
2024-01-30 10:23:26 +0500

    update base html

--- base/models.py
+++ base/models.py
@@ -9,7 +9,7 @@ class ContactForm(models.Model):
     last_name = models.CharField(_("Last name"), blank=True, max_length=55)
     email = models.CharField(_("Email"), blank=True, max_length=55)
     subject = models.CharField(_("Subject"), blank=True, max_length=55)
-    message = models.CharField(_("Subject"), blank=True, max_length=500)
+    message = models.CharField(_("Message"), blank=True, max_length=500)
     created_at = models.DateTimeField(_("Created at"), default=datetime.now)
 
     class Meta:
